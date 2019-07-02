60881d1769e40017abac586d097cea99dd7c539e
nick <290902391@qq.com>
2019-07-02 18:06:18 +0800

    优化Calendar生成逻辑

--- src/main/java/com/hxzy/biz/Test2.java
+++ src/main/java/com/hxzy/biz/Test2.java
@@ -12,27 +12,22 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Test2 {
-    private static long current = System.currentTimeMillis(); //当前时间
-
     public static void main(String[] args) {
         List<Date> list = new ArrayList<>();
 
-        Calendar c = Calendar.getInstance();
-        c.setTime(converMills2Date(current));
+        Calendar c = Calendar.getInstance(); //获取当前日期
 
         int actualMaximum = c.getActualMaximum(Calendar.DATE); //每个月的最后一天
         int actualMinimum = c.getActualMinimum(Calendar.DATE); //每个月的第一天
 
-        c.set(Calendar.DATE, actualMinimum);// 每个月的1号为起点
+        c.set(Calendar.DATE, actualMinimum);// 每个月的1号为值班起点
 
-        current = c.getTimeInMillis(); //得到第一天的毫秒
-        list.add(converMills2Date(current)); //将毫秒转换为日期对象
+        list.add(converMills2Date(c.getTimeInMillis())); //将毫秒转换为日期对象
 
         List<Date> list2 = Stream.generate(() -> {
-            Date instance = converMills2Date(current);
-            c.setTime(instance);
+
             c.add(Calendar.DATE, 1);
-            current = c.getTimeInMillis();
+
             return c.getTime();
         })
                 .limit(actualMaximum - 1)
