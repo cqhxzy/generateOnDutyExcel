fdd1dd601c5839f55278c015f82aad02fd467d30
nick <290902391@qq.com>
2019-07-04 13:35:42 +0800

    添加员工值班模板，解析模板人员信息

--- src/main/java/com/hxzy/util/XmlUtil.java
+++ src/main/java/com/hxzy/util/XmlUtil.java
@@ -1,11 +1,16 @@
 package com.hxzy.util;
 
+import com.hxzy.bean.DutyItem;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
+import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class XmlUtil {
     private static final String DEFAULT_PATH = "/dutyList.xml";
@@ -22,4 +27,25 @@ public class XmlUtil {
         return null;
     }
 
+    public static List<DutyItem> getDutyItems(){
+        Document document = getDocument();
+        Element root = document.getRootElement();
+        List<Element> elements = root.elements();
+        List<DutyItem> dutyItems = elements.stream().map(t -> {
+            String date = t.element("date").getText();
+            List<Element> list = t.elements("list");
+            DutyItem item = new DutyItem();
+            @SuppressWarnings("unchecked")
+            Optional<List<String>> first = list.stream().map(Element::elements).map(k ->
+                    (List<String>) k.stream().
+                            map(h ->
+                                    ((Element) h).getText()
+                            ).collect(Collectors.toList())
+            ).findFirst();
+            item.setDutyDate(date);
+            item.setNames(first.orElse(null));
+            return item;
+        }).collect(Collectors.toList());
+        return dutyItems;
+    }
 }
